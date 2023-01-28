b05ce4257a12060e9c6cb572945da0d3ea78728c
Jake Waldron <jakewaldron@gmail.com>
2023-01-28 09:01:24 -0500

    show feedback on guesses

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { checkGuess } from '../../game-helpers';
 import { range } from '../../utils';
 
-function Guess({ guess }) {
+function Guess({ guess, answer }) {
+	const checkedGuess = checkGuess(guess, answer);
+
 	return (
 		<p className="guess">
-			{range(5).map((letterIndex) => (
-				<span key={letterIndex} className="cell">
-					{guess?.[letterIndex] || ''}
-				</span>
-			))}
+			{range(5).map((letterIndex) => {
+				const letterInfo = checkedGuess?.[letterIndex];
+				return (
+					<span key={letterIndex} className={letterInfo ? `${letterInfo.status} cell` : 'cell'}>
+						{letterInfo ? letterInfo.letter : ''}
+					</span>
+				);
+			})}
 		</p>
 	);
 }
